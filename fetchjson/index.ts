d03e9175d83b51cfefdfdaf2e4f084a1601cc840
CarrieKroutil <CarrieKroutil@gmail.com>
2025-08-08 05:58:38 -0700

    cleanup

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -3,15 +3,6 @@ import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 // Fetch JSON data from the URL via HTTP GET request that returns a Promise
-
-axios.get(url).then(response => {
-  const todo = response.data;
-    console.log(todo.id, todo.title);
-});
-
-/* Basic HTTP GET Example
-// Fetch JSON data from the URL via HTTP GET request that returns a Promise
 axios.get(url).then(response => {
   console.log(response.data);
 });
-*/
